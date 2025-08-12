57d4aa0719b214b072d8815ae40d0b557a94b14a
AmirReza <fgg32437@gmail.com>
2025-08-12 23:03:39 +0330

    add config

--- api-config/api-url.ts
+++ api-config/api-url.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  //apiUrl: 'http://localhost:5000',
-  apiUrl: ''
-  //apiUrl: 'https://localhost:44322',
-  //apiUrl: 'http://localhost:8085',
+  apiUrl: 'https://localhost:44322',
+  // apiUrl: 'http://localhost:8085',
 };
