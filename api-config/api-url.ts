16865f0fdc22e79e90949aa25a9cff2ed5838a95
Mohammad Sadegh Montazeri <101090751+msm1984@users.noreply.github.com>
2025-08-12 23:34:08 +0330

    Update api-url.ts

--- api-config/api-url.ts
+++ api-config/api-url.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://localhost:44322',
-  // apiUrl: 'http://localhost:8085',
+  // apiUrl: 'https://localhost:44322',
+  apiUrl: 'http://localhost:8085',
 };
